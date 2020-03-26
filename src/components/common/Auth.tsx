319a6c0c198f41dc70acb9cd8c012d6411167607
YukiSato <yukiyukiamam@gmail.com>
2020-03-26 17:26:52 +0900

    ログイン処理の追加

---
+++ src/components/common/Auth.tsx
@@ -0,0 +1,74 @@
+import React, { useContext, useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import useReactRouter from "use-react-router";
+
+import { AuthContext } from "../common/context/provider";
+
+type AuthProps = {
+  children: React.ReactNode
+};
+
+export const AuthComponent: React.FC<AuthProps> = ({ children }) => {
+  const path = useReactRouter().location.pathname;
+
+  const { authState, setAuth } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+  const [logged, setLogged] = useState(false);
+
+  const fetchCurrentUser = async () => {
+    let TokenInCookie = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + TokenInCookie;
+    
+    if(TokenInCookie == "") return;
+    
+    let currentUser = await axios
+      .get(`/api/v1/users/me`)
+      .then(result => result.data)
+      .catch(error => {
+        console.log(error);
+      })
+
+    if (!currentUser) throw "Login Error";
+
+    return currentUser;
+  }
+
+  const login = useCallback(async () => {
+    try {
+      fetchCurrentUser().then(res => {
+        if (res) {
+          setAuth({
+            isLogged: true,
+            user: res
+          });
+          setLogged(true);
+        } else {
+          setLogged(false);
+        }
+      }).catch(error => {
+        console.log(error);
+        setLogged(false);
+      }).finally(() => {
+        setLoading(false);
+      });
+    } catch (error) {
+      console.log(error);
+      axios.defaults.headers.common['Authorization'] = '';
+      setLogged(false);
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    login();
+  }, [path]);
+
+  return (
+    loading ? <div></div> :
+      logged ?
+        (<div>{children}</div>) :
+        (<Redirect to={"/account/login"}/>)
+  );
+};
--- src/components/pages/account/Login.tsx
+++ src/components/pages/account/Login.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useRef, useContext, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { AuthContext } from "../../common/context/provider";
 
 const linkStyle = {
   textAlign: "center" as "center",
@@ -12,28 +14,95 @@ const linkStyle = {
 };
 
 export const AccountLogin: React.FC = (props: any) => {
+  const { authState, setAuth } = useContext(AuthContext);
+  const mailRef = useRef(document.createElement("input"));
+  const passwordRef = useRef(document.createElement("input"));
+  const buttonRef = useRef(document.createElement("button"));
+
   const send = async () => {
+    try {
+      if(!mailRef.current.value || !passwordRef.current.value) return;
+      const user = await axios.post(
+        '/api/v1/users/sign_in',
+        {
+          email: mailRef.current.value,
+          password: passwordRef.current.value
+        }
+      )
+      .then(res => res.data)
+      .catch(err => console.log(err));
+      console.log(user);
+      // set Authorization header
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + user.token;
+      //cookieに保存
+      document.cookie = `token=${user.token}`
+      
+      setAuth({
+        isLogged: true,
+        user: user
+      });
 
-    // set Authorization header
-    // axios.defaults.headers.common['Authorization'] = 'Bearer ' + user.signInUserSession.idToken.jwtToken;
+      if(authState.isLogged){
+        console.log("login send button to /home");
+        props.history.push({
+          pathname: "/home"
+        })
+      }
 
+      return;
 
-    props.history.push({
-      pathname: "/home"
-    });
+    } catch (err) {
+      axios.defaults.headers.common['Authorization'] = '';
+      console.log(err);
+    }
   }
+
+  const setUserData = useCallback(async () => {
+    try {
+      let TokenInCookie = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+      if (!TokenInCookie) return;
+      // set Authorization header
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + TokenInCookie;
+
+      const user = await axios.get(
+        '/api/v1/users/me'
+      )
+      .then(res => res.data)
+      .catch(err => console.log(err));
+
+      setAuth({
+        isLogged: true,
+        user: user
+      });
+
+      if(authState.isLogged){
+        console.log("login set user data function to /home");
+        props.history.push({
+          pathname: "/home"
+        });
+      }
+      return;
+    } catch (err) {
+      axios.defaults.headers.common['Authorization'] = '';
+      console.log(err);
+    }
+  }, [authState, setAuth, props.history])
+
+  useEffect(() => {
+    setUserData();
+  }, [setUserData]);
 
   return (
     <div>
       <h1 className="title">ideaStokkerを始める</h1>
       <div>
         <label>メールアドレス</label>
-        <input type="text" placeholder="メールアドレス"/>
+        <input ref={ mailRef } type="text" placeholder="メールアドレス"/>
         <label>パスワード</label>
-        <input type="password" placeholder="パスワード"/>
+        <input ref={ passwordRef } type="password" placeholder="パスワード"/>
       </div>
       <p>
-        <button onClick={ send }>
+        <button ref={ buttonRef } onClick={ send }>
           ログイン
         </button>
       </p>
