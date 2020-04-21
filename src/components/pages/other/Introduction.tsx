af3d2960f51c57a686ce8dbe977b7f9f7116dcad
aizawa-haru <aizawadesign13@gmail.com>
2020-04-21 15:38:01 +0900

    [add]イントロページ途中

--- src/components/pages/other/Introduction.tsx
+++ src/components/pages/other/Introduction.tsx
@@ -4,7 +4,23 @@ import { Link } from 'react-router-dom';
 export const Introduction: React.FC = () => {
   return (
     <div>
-      <h1>サービス紹介ページ</h1>
+        <header className="page-header">
+            <h1 className="header-appname">STOCKROOM</h1>
+            <ul className="header-link-container">
+                <li className="header-link">ログイン</li>
+                <li className="header-link">会員登録</li>
+            </ul>
+        </header>
+        <body>
+            <section className="firstview-section">
+                <h2>STOCKROOM</h2>
+            </section>
+            <section className="about-section"></section>
+            <section className="recommend-section"></section>
+            <section className="features-section"></section>
+            <section className="demand-section"></section>
+        </body>
+        <footer className="page-footer"></footer>
       <style jsx>{`
       //example 好きに変えていいよ
         div {
