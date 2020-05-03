bf3f55189d6047db9ca60d6b8027792b0a76aabb
aizawa-haru <aizawadesign13@gmail.com>
2020-05-03 17:04:10 +0900

    [add]レイアウトの変更

--- src/components/pages/account/Top.tsx
+++ src/components/pages/account/Top.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import useReactRouter from "use-react-router";
 import { Link } from 'react-router-dom';
+import Logo from './../../../../src/components/images/logo.svg'
 
 const createLinkStyle = {
   textAlign: "center" as "center",
@@ -9,6 +10,7 @@ const createLinkStyle = {
   lineHeight: "30px",
   cursor: "pointer",
   border: "3px solid #FEB342",
+  borderRadius: "4px",
   backgroundColor: "#FEB342",
   marginBottom: "10px"
 };
@@ -19,7 +21,8 @@ const googleLinkStyle = {
   lineHeight: "30px",
   cursor: "pointer",
   border: "3px solid #FEB342",
-  marginBottom: "10px"
+  marginBottom: "10px",
+  borderRadius: "4px"
 };
 const loginLinkStyle = {
   textAlign: "center" as "center",
@@ -27,6 +30,7 @@ const loginLinkStyle = {
   height: "30px",
   lineHeight: "30px",
   cursor: "pointer",
+  borderRadius: "4px",
   border: "3px solid #C4C4C4",
   backgroundColor: "#C4C4C4",
   marginBottom: "10px"
@@ -37,43 +41,44 @@ const detailLinkStyle = {
   height: "30px",
   lineHeight: "30px",
   cursor: "pointer",
+  borderRadius: "4px"
 };
 
 export const Top: React.FC = () => {
   return (
-    <div className="container">
-      <div className="title">
-        <img src="a" />
-        <p className="image-text">ひらめきをストックしよう</p>
-      </div>
-      <p className="start-text">STOCKROOMを始める</p>
-      <div className="login-container">
-        <Link to='/account/create' style={createLinkStyle}>アカウントを作成</Link>
-        <Link to='/' style={googleLinkStyle}>googleアカウントで登録/ログイン</Link>{/* FIXME このリンクは変更になる可能性があるのであとで編集してほしい */}
-        <Link to='/account/login' style={loginLinkStyle}>ログイン</Link>
-      </div>
-      <div>
-        <h2 className="intro-title">What is STOCKROOM ?</h2>
-        <p className="intro-content">sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample</p>
-        <Link to='/introduction' style={detailLinkStyle}>詳しくはこちら➡︎</Link>
-      </div>
+    <div>
+        <div className="container">
+            <div className="title">
+                <img className="top-logo" src={ Logo } />
+            </div>
+            <p className="start-text">STOCKROOMを始める</p>
+            <div className="login-container">
+                <Link to='/account/create' style={createLinkStyle}>アカウントを作成</Link>
+                <Link to='/' style={googleLinkStyle}>googleアカウントで登録/ログイン</Link>{/* FIXME このリンクは変更になる可能性があるのであとで編集してほしい */}
+                <Link to='/account/login' style={loginLinkStyle}>ログイン</Link>
+            </div>
+        </div>
+        <div className="introduction">
+            <h2 className="intro-title">What is STOCKROOM ?</h2>
+            <p className="intro-content">
+                STOCKROOMとは、「ひらめき」を簡単にストック、整理できるメモアプリです。ひらめきを一つの場所にまとめることで、スムーズで柔軟な創造活動をサポートします！
+            </p>
+            <Link to='/introduction' style={detailLinkStyle}>詳しくはこちら➡︎</Link>
+        </div>
       <style jsx>{`
         .container {
-          padding: 1rem 1.5rem;
+          padding: 40px 28px;
+          box-sizing: border-box;
         }
 
         .title {
-          position:relative;
-          margin-bottom: 50px;
-          height: 150px; 
+          margin-bottom: 100px;
+          padding-top: 60px;
+          padding-left: 12px;
         }
-
-        .title img {
-          height: 80px;
-          width: 80px;
-          position: absolute;
-          top: 50%;
-          transform: translateY(-50%)
+        .top-logo{
+            width: 70px;
+            height: auto;
         }
 
         .image-text {
@@ -95,6 +100,12 @@ export const Top: React.FC = () => {
           margin-bottom: 100px;
         }
 
+        .introduction{
+            text-align: justify;
+            position: fixed;
+            padding: 40px 28px;
+            box-sizing: border-box;
+        }
         .intro-title {
           font-weight: bold;
           font-size: 18px;
