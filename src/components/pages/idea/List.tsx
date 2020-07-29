3bb31a708d7b32b6b2f3423d23cc8b68188e66f6
相澤春伽 <aizawadesign13@gmail.com>
2020-07-29 19:23:12 +0900

    [test]list-scroll

--- src/components/pages/idea/List.tsx
+++ src/components/pages/idea/List.tsx
@@ -159,6 +159,7 @@ export const IdeaList: React.FC = (props: any) => {
         setQuery={setTagSearchQuery}
         currentQuery={tagSearchQuery}
       />
+      <div className="bg-gray"></div>
       <div className={`container`}>
         <InfiniteScroll
           pageStart={1}
@@ -233,18 +234,27 @@ export const IdeaList: React.FC = (props: any) => {
         main::-webkit-scrollbar {
           display:none;
         }
+        .bg-gray{
+          width: 100%;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          position: fixed;
+          background-color: #F5F5F5;
+        }
         .container {
           height: calc(100vh - 32px);
           width: 100%;
-          padding: 48px 12px 400px 12px;
+          padding: 48px 12px 180px 12px;
           box-sizing: border-box;
           overflow-y: scroll;
           background-color: #F5F5F5;
           max-width: 1000px;
-          margin: 108px auto 0 auto;
-          display: flex;
-          flex-wrap: wrap;
-          justify-content: space-around;
+          top: 108px;
+          position: fixed;
+          left: 50%;
+          transform: translateX(-50%);
+          -webkit- transform: translateX(-50%);
         }
         @media (max-width: 1000px){
           .container {
